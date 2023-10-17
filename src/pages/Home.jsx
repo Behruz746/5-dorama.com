200f10b490c4fd859d4860da318c2f98eca458e4
Behruz <>
2023-10-17 13:27:05 +0500

    test00

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
       <HeaderPage image="./images/png/backound__img01.png" />
       <Sliders url="https://kodikapi.com/list?token=7e04e50106ab3a654bef8c638ea36fa8&with_episodes=true&with_material_data=true&limit=50&lgbt=false&types=foreign-serial,soviet-cartoon,oreign-movie&year=2021,2022,2023&kinopoisk_rating=5-8&imdb_rating=5-8" title="Популярные сейчас" />
       <Sliders url="https://kodikapi.com/list?token=7e04e50106ab3a654bef8c638ea36fa8&with_episodes=true&with_material_data=true&limit=50&lgbt=false&types=foreign-serial,soviet-cartoon,oreign-movie&year=2021,2022,2023" title="Новинки" />
-      <Sliders url="https://kodikapi.com/list?token=7e04e50106ab3a654bef8c638ea36fa8&with_episodes=true&with_material_data=true&limit=13&lgbt=false&types=anime&sort=year" title="Фильмы" />
+      <Sliders url="https://kodikapi.com/list?token=7e04e50106ab3a654bef8c638ea36fa8&with_episodes=true&with_material_data=true&limit=100&lgbt=false&types=anime&sort=kinopoisk_rating&year=2015,2016,2017,2018,2019,2020," title="Фильмы" />
     </div>
   );
 }
