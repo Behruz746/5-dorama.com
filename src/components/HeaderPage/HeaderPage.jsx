ba63a59f9c534872cddb3f61732fbbf677bf26bf
Behruz <>
2023-10-21 14:12:03 +0500

    test00

--- src/components/HeaderPage/HeaderPage.jsx
+++ src/components/HeaderPage/HeaderPage.jsx
@@ -4,17 +4,16 @@ import { NavLink } from "react-router-dom";
 
 import "./styles.scss";
 
-function HeaderPage({image, containerBlur}) {
+function HeaderPage({ image, containerBlur }) {
   return (
     <section className="Home__HaderPage w-full">
+      <a href="./kodik.txt">link</a>
+
       <div className={`container ${containerBlur}`}>
         <NavLink to="*">
           <div className="HaderPage__image">
             <div className="image">
-              <img
-                src={image} 
-                alt="background image"
-              />
+              <img src={image} alt="background image" />
             </div>
           </div>
         </NavLink>
