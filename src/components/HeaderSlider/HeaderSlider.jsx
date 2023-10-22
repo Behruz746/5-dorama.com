94ffa7bfed0aee56cc3a710e80f4a97c6dcdad53
Behruz <>
2023-10-22 20:23:41 +0500

    added new style for header slider compoents

--- src/components/HeaderSlider/HeaderSlider.jsx
+++ src/components/HeaderSlider/HeaderSlider.jsx
@@ -1,6 +1,7 @@
 import Slider from "react-slick";
 import { NavLink } from "react-router-dom";
 import { dataSlider } from "../../data/data";
+import { v4 as uuidv4 } from "uuid";
 
 import "./styles.scss";
 
@@ -9,12 +10,12 @@ function HeaderSlider({pNone}) {
     className: "slider variable-width",
     dots: false,
     infinite: true,
-    speed: 1200,
+    speed: 1200, 
     slidesToShow: 1,
     slidesToScroll: 1,
     pauseOnHover: true,
     variableWidth: false,
-    adaptiveHeight: true,
+    // adaptiveHeight: true,
     arrows: true,
   };
 
@@ -23,7 +24,7 @@ function HeaderSlider({pNone}) {
       <div className="container" style={{padding: `${pNone}`}}>
           <Slider {...settings}>
             {dataSlider.map((data) => (
-              <div className="slider__card">
+              <div className="slider__card" key={uuidv4()}>
                 <NavLink className="slider__link" to={data.link}>
                   <img
                     className="slider__card-img"
--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -4,16 +4,18 @@ import AppContext from "../AppContext";
 function VideoPlayer() {
   const { isVideoLink } = useContext(AppContext);
   return (
-    <div>
-      <iframe
-        className="videoPlayer"
-        src={localStorage.getItem('dataLink')} // video link form locakStorage.getItem('data');
-        width="846px"
-        height="476px"
-        frameBorder="0"
-        allowFullScreen
-        allow="autoplay *; fullscreen *"
-      ></iframe>
+    <div className="VideoPlayer w-full">
+      <div className="container">
+        <iframe
+          className="videoPlayer"
+          src={localStorage.getItem("dataLink")} // video link form locakStorage.getItem('data');
+          width="846px"
+          height="476px"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay *; fullscreen *"
+        ></iframe>
+      </div>
     </div>
   );
 }
