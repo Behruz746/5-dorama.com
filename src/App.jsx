3a6d39ae6c290fac0bafec94dde74ee8623cc51b
Behruz <>
2023-10-23 19:24:37 +0500

    test01

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,7 @@ function App() {
   }
 
   useEffect(() => {
-    // const time = 10000 * (6 * 5);
-    const time = 1000;
+    const time = 10000 * (6 * 5);
 
     setTimeout(() => {
       setModalSec(true);
