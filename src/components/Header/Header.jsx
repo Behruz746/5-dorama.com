3f2a2cc734728a74fc2aa3f9239e43dd3513c679
Behruz <>
2023-10-26 11:04:28 +0500

    test00

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -33,24 +33,28 @@ function Header() {
 
   const Svg01 = () => (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
     >
-      <g id="Search" clipPath="url(#clip0_87_40)">
-        <path
-          id="Vector"
-          d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
-          fill="#768C9E"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_87_40">
-          <rect width="24" height="24" fill="white" />
-        </clipPath>
-      </defs>
+      <circle
+        cx="8.99998"
+        cy="9"
+        r="8"
+        stroke="#768C9E"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.5 14.958L19.5 19.958"
+        stroke="#768C9E"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 
