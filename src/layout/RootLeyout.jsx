908e6b359e2d6d4fa08af52ef5b1bd0f0430c376
Behruz <>
2023-11-14 22:17:02 +0500

    test20

--- src/layout/RootLeyout.jsx
+++ src/layout/RootLeyout.jsx
@@ -33,7 +33,7 @@ function RootLeyout() {
         <Menu />
         <Outlet />
       </main>
-      {/* <AnimationBlock /> */}
+      <AnimationBlock />
       <Analytics />
       <MenuMobile />  
     </>
