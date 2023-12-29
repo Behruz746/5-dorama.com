0ed38f41270f06440950910a29208ea5ed711a06
Behruz <madaminovbehruz746@gmail.com>
2023-12-29 13:05:22 +0500

    test: loading lazy 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { lazy } from "react";
 
 import "./sass/main.scss";
-import "react-lazy-load-image-component/src/effects/blur.css";
 
 // Hooks
 import { useState, useEffect, useRef } from "react";
@@ -11,87 +9,51 @@ import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 // Context
-const AppContext = lazy(() => import("./AppContext"));
+import AppContext from "./AppContext";
 
-// Layouts
-const RootLeyout = lazy(() => import("./layout/RootLeyout"));
-const RootPlayer = lazy(() => import("./layout/RootPlayer"));
-const RootCategorys = lazy(() => import("./layout/RootCategorys"));
-const RootMovies = lazy(() => import("./layout/RootMovies"));
-const RootSerail = lazy(() => import("./layout/RootSerail"));
+// Layout
+import RootLeyout from "./layout/RootLeyout";
+import RootPlayer from "./layout/RootPlayer";
+import RootCategorys from "./layout/RootCategorys";
+import RootMovies from "./layout/RootMovies";
+import RootSerail from "./layout/RootSerail";
 
 // Pages
-const Home = lazy(() => import("./pages/Home"));
-const Shorts = lazy(() => import("./pages/Shorts"));
-const Dramas = lazy(() => import("./pages/Dramas"));
-const Movies = lazy(() => import("./pages/Movies"));
-const Actor = lazy(() => import("./pages/Actor"));
-const VideoPlayer = lazy(() => import("./pages/VideoPlayer"));
-const Document = lazy(() => import("./pages/Document"));
-const About = lazy(() => import("./pages/About"));
-const Contact = lazy(() => import("./pages/Contact"));
-const PageNotFound = lazy(() => import("./pages/pageErrorFound/PageNotFound"));
-const PopularNow = lazy(() => import("./pages/pageCategorys/PopularNow"));
-const NewMovies = lazy(() => import("./pages/pageCategorys/NewMovies"));
-const AnimeMovie = lazy(() => import("./pages/pageCategorys/AnimeMovie"));
+import Home from "./pages/Home";
+import Shorts from "./pages/Shorts";
+import Dramas from "./pages/Dramas";
+import Movies from "./pages/Movies";
+import Actor from "./pages/Actor";
+import VideoPlayer from "./pages/VideoPlayer";
+import Document from "./pages/Document";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/pageErrorFound/PageNotFound";
+import PopularNow from "./pages/pageCategorys/PopularNow";
+import NewMovies from "./pages/pageCategorys/NewMovies";
+import AnimeMovie from "./pages/pageCategorys/AnimeMovie";
 
-// Movies category pages
-const ComedyMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/ComedyMPage")
-);
-const DramaMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/DramaMPage")
-);
-const ActionMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/ActionMPage")
-);
-const BiographyMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/BiographyMPage")
-);
-const MilitaryMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/MilitaryMPage")
-);
-const FantasticMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/FantasticMPage")
-);
-const HorrorMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/HorrorMPage")
-);
-const CrimeMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/CrimeMPage")
-);
-const AnimeMPage = lazy(() =>
-  import("./pages/pageCategorys/moviePages/AnimeMPage")
-);
+// Movies catigoriy pages
+import ComedyMPage from "./pages/pageCategorys/moviePages/ComedyMPage";
+import DramaMPage from "./pages/pageCategorys/moviePages/DramaMPage";
+import ActionMPage from "./pages/pageCategorys/moviePages/ActionMPage";
+import BiographyMPage from "./pages/pageCategorys/moviePages/BiographyMPage";
+import MilitaryMPage from "./pages/pageCategorys/moviePages/MilitaryMPage";
+import FantasticMPage from "./pages/pageCategorys/moviePages/FantasticMPage";
+import HorrorMPage from "./pages/pageCategorys/moviePages/HorrorMPage";
+import CrimeMPage from "./pages/pageCategorys/moviePages/CrimeMPage";
+import AnimeMPage from "./pages/pageCategorys/moviePages/AnimeMPage";
 
-// Serial category pages
-const ComedySPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/ComedySPage")
-);
-const DramaSPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/DramaSPage")
-);
-const ActionSPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/ActionSPage")
-);
-const BiographySPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/BiographySPage")
-);
-const MilitarySPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/MilitarySPage")
-);
-const FantasticSPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/FantasticSPage")
-);
-const HorrorSPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/HorrorSPage")
-);
-const CrimeSPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/CrimeSPage")
-);
-const AnimeSPage = lazy(() =>
-  import("./pages/pageCategorys/serialPages/AnimeSPage")
-);
+// Serial catigoriy pages
+import ComedySPage from "./pages/pageCategorys/serialPages/ComedySPage";
+import DramaSPage from "./pages/pageCategorys/serialPages/DramaSPage";
+import ActionSPage from "./pages/pageCategorys/serialPages/ActionSPage";
+import BiographySPage from "./pages/pageCategorys/serialPages/BiographySPage";
+import MilitarySPage from "./pages/pageCategorys/serialPages/MilitarySPage";
+import FantasticSPage from "./pages/pageCategorys/serialPages/FantasticSPage";
+import HorrorSPage from "./pages/pageCategorys/serialPages/HorrorSPage";
+import CrimeSPage from "./pages/pageCategorys/serialPages/CrimeSPage";
+import AnimeSPage from "./pages/pageCategorys/serialPages/AnimeSPage";
 
 function App() {
   const [isToggle, setIsToggle] = useState(true);
@@ -121,7 +83,7 @@ function App() {
     inputEl.forEach((item) => {
       item.addEventListener("change", (e) => {
         setIsUrl(
-          `https://kodikapi.com/search?token=7e04e50106ab3a654bef8c638ea36fa8&title=${e.target.value}&with_material_data=true&lgbt=false&limit=15&countries=Япония,Корея Южная,Китай,тайлант`
+          `https://kodikapi.com/search?token=7e04e50106ab3a654bef8c638ea36fa8&title=${e.target.value}&with_material_data=true&lgbt=false&limit=15`
         );
         setLoadShearch(false);
 
@@ -154,7 +116,7 @@ function App() {
       if (window.screen.width <= 950) {
         setIsToggle(false);
       } else {
-        setIsToggle(false);
+        setIsToggle(true);
       }
     }
     window.addEventListener("resize", handleResize);
